55c73b7fc3ba5fff6b5249e07bc583b5366c6a9f
Koen Braham <koen.braham@alten.nl>
2023-11-20 17:06:21 +0100

    Add exercise 1

---
+++ exercise_1_a_little_rust_in_cpp/rust_calculator/src/lib.rs
@@ -0,0 +1,17 @@
+// These functions are missing some prefix
+
+fn rust_whothis() {
+    println!("Hi I'm a rust calculator!");
+}
+
+fn rust_add(x: u16, y: u16) -> u16 {
+    x + y
+}
+
+fn rust_subtract(x: i32, y: i32) -> i32 {
+    (x - y) as i32
+}
+
+fn rust_multiply(x: i32, y: i32) -> i32 {
+    x * y
+}
